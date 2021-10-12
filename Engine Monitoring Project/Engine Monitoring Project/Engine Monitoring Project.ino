e63c4e418e39ee19d70d0fcc3117cb72b171935f
TristonWa <66381392+TristonWa@users.noreply.github.com>
2021-10-11 19:00:09 -0500

    This is just the base of the due code

---
+++ Engine Monitoring Project/Engine Monitoring Project/Engine Monitoring Project.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Engine_Monitoring_Project.ino
+ Created:	10/11/2021 6:59:39 PM
+ Author:	tsw15
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
